3b5c26f6b9325ba2d1a120e6b009060f05a75a13
Laura Vingada <laura@vingada.com>
2017-03-11 19:04:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../public/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Nasa Photo of the Day</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
